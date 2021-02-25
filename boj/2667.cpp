270822e85f7fb4bc84a2e54cfc1c05de869b9ab5
SeokJiwon <tjrwldnjs103@naver.com>
2021-02-25 23:46:22 +0900

    210225

---
+++ boj/2667.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int map[25][25] = {0};
+bool visited[25][25];
+
+int dx[] = {0, 1, 0, -1};
+int dy[] = {-1, 0, 1, 0};
+
+int dfs(int x, int y) {
+	visited[x][y] = true;
+	int count = 0;
+	for (int i = 0; i < 4; i++) {
+		int xx = x + dx[i];
+		int yy = y + dy[i];
+
+		if (xx >= 0 && yy >= 0 && xx < N && yy < N && map[xx][yy] == 1 && !visited[xx][yy]) {
+			visited[xx][yy] = true;
+			count++;
+			count += dfs(xx, yy);
+		}
+	}
+	return count;
+}
+int main(void) {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%1d", &map[i][j]);
+		}
+	}
+
+	vector<int> answer;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 1 && !visited[i][j]) answer.push_back(dfs(i, j));
+		}
+	}
+
+	sort(answer.begin(), answer.end());
+	int size = 0;
+	size = answer.size();
+	cout << answer.size() << endl;
+	for (int v : answer) {
+		cout << v+1 << endl;
+	}
+}
